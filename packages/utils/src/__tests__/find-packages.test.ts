88690b4a0d6e646f947eb6a8d0e69188ff238b26
leecheneler <lacheneler@gmail.com>
2020-05-04 17:21:11 +0100

    added findPackages function

---
+++ packages/utils/src/__tests__/find-packages.test.ts
@@ -0,0 +1,41 @@
+import mockFs from "mock-fs";
+import { findMonoRepo } from "../find-mono-repo";
+import { findPackages } from "../find-packages";
+
+describe("findPackages", () => {
+  afterEach(() => {
+    mockFs.restore();
+  });
+
+  it("should find all packages in the mono repo", async () => {
+    const pkgA = { name: "a", version: "1.0.0", scripts: { start: "node" } };
+    const pkgB = { name: "b", version: "2.1.0", dependencies: { abc: "16" } };
+    const pkgC = { name: "c", version: "1.2.3", description: "hello world" };
+    const monoRepoPkg = {
+      private: true,
+      workspaces: ["packages/**", "some/deeper/alt-packages/*"],
+    };
+
+    mockFs({
+      "/package.json": JSON.stringify(monoRepoPkg),
+      "/packages/a/package.json": JSON.stringify(pkgA),
+      "/packages/b/package.json": JSON.stringify(pkgB),
+      "/some/deeper/alt-packages/c/package.json": JSON.stringify(pkgC),
+    });
+
+    expect(await findPackages(await findMonoRepo())).toEqual([
+      {
+        dir: "/packages/a",
+        json: pkgA,
+      },
+      {
+        dir: "/packages/b",
+        json: pkgB,
+      },
+      {
+        dir: "/some/deeper/alt-packages/c",
+        json: pkgC,
+      },
+    ]);
+  });
+});
--- packages/utils/src/find-packages.ts
+++ packages/utils/src/find-packages.ts
@@ -1,5 +1,34 @@
-import { Package } from "./types";
+import path from "path";
+import fs from "fs-extra";
+import glob from "glob-promise";
+import { MonoRepo, MonoRepoPackage, Package } from "./types";
 
-export const findPackages = (): Package[] => {
-  return [];
+export const findPackages = async (
+  monoRepo: MonoRepo
+): Promise<MonoRepoPackage[]> => {
+  // Build globs to packages package.json files
+  const packageJsonGlobs = monoRepo.packageGlobs.map((x) =>
+    path.resolve(monoRepo.dir, x, "package.json")
+  );
+
+  // Find all paths to package.jsons via globs
+  const globResults = await Promise.all(packageJsonGlobs.map((g) => glob(g)));
+
+  // Need to flatten glob results into a simple array of filepaths
+  const packageJsonFilepaths = globResults
+    .reduce((acc, next) => [...acc, ...next], [])
+    .filter((x) => !x.includes("node_modules"));
+
+  // Load all file contents into a JS object, it's JSON so serializes properly
+  const packages = await Promise.all(
+    packageJsonFilepaths.map((x: string): Promise<Package> => fs.readJson(x))
+  );
+
+  // Apply package meta data on the way out
+  return packages.map(
+    (x, i): MonoRepoPackage => ({
+      dir: packageJsonFilepaths[i].replace("/package.json", ""),
+      json: x,
+    })
+  );
 };
