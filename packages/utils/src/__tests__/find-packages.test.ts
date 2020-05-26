aa1a718aa6a3b0ff53535c8ab5165e6cdacc5c3e
leecheneler <lacheneler@gmail.com>
2020-05-26 18:26:38 +0100

    added dependency order support to getPackages

--- packages/utils/src/__tests__/find-packages.test.ts
+++ packages/utils/src/__tests__/find-packages.test.ts
@@ -7,7 +7,7 @@ describe("findPackages", () => {
     mockFs.restore();
   });
 
-  it("should find all packages in the mono repo without duplicates", async () => {
+  it("should find all packages in the mono repo without duplicates (alphabetical order by default)", async () => {
     const pkgA = { name: "a", version: "1.0.0", scripts: { start: "node" } };
     const pkgB = { name: "b", version: "2.1.0", dependencies: { abc: "16" } };
     const pkgC = { name: "c", version: "1.2.3", description: "hello world" };
@@ -37,5 +37,107 @@ describe("findPackages", () => {
         json: pkgC,
       },
     ]);
+  });
+
+  it("should find all packages in the mono repo without duplicates (dependency order)", async () => {
+    const pkgA = {
+      name: "a",
+      version: "1.0.0",
+      dependencies: { b: "1.0.0", c: "1.0.0" },
+    };
+    const pkgB = { name: "b", version: "1.0.0", dependencies: { c: "1.0.0" } };
+    const pkgC = {
+      name: "c",
+      version: "1.0.0",
+      dependencies: { d: "1.0.0", f: "1.0.0" },
+    };
+    const pkgD = {
+      name: "d",
+      version: "1.0.0",
+      dependencies: { e: "1.0.0", f: "1.0.0" },
+    };
+    const pkgE = { name: "e", version: "1.0.0", dependencies: {} };
+    const pkgF = { name: "f", version: "1.0.0", dependencies: {} };
+    const monoRepoPkg = {
+      private: true,
+      workspaces: ["packages/**"],
+    };
+
+    mockFs({
+      "/package.json": JSON.stringify(monoRepoPkg),
+      "/packages/a/package.json": JSON.stringify(pkgA),
+      "/packages/b/package.json": JSON.stringify(pkgB),
+      "/packages/c/package.json": JSON.stringify(pkgC),
+      "/packages/d/package.json": JSON.stringify(pkgD),
+      "/packages/e/package.json": JSON.stringify(pkgE),
+      "/packages/f/package.json": JSON.stringify(pkgF),
+    });
+
+    expect(
+      await findPackages(await findMonoRepo(), { order: "dependency" })
+    ).toEqual([
+      {
+        dir: "/packages/e",
+        json: pkgE,
+      },
+      {
+        dir: "/packages/f",
+        json: pkgF,
+      },
+      {
+        dir: "/packages/d",
+        json: pkgD,
+      },
+      {
+        dir: "/packages/c",
+        json: pkgC,
+      },
+      {
+        dir: "/packages/b",
+        json: pkgB,
+      },
+      {
+        dir: "/packages/a",
+        json: pkgA,
+      },
+    ]);
+  });
+
+  it("should throw when there is a circular dependency (a -> b, b -> a)", async () => {
+    const pkgA = { name: "a", version: "1.0.0", dependencies: { b: "1.0.0" } };
+    const pkgB = { name: "b", version: "1.0.0", dependencies: { a: "1.0.0" } };
+    const monoRepoPkg = {
+      private: true,
+      workspaces: ["packages/**"],
+    };
+
+    mockFs({
+      "/package.json": JSON.stringify(monoRepoPkg),
+      "/packages/a/package.json": JSON.stringify(pkgA),
+      "/packages/b/package.json": JSON.stringify(pkgB),
+    });
+
+    await expect(
+      async () =>
+        await findPackages(await findMonoRepo(), { order: "dependency" })
+    ).rejects.toThrowError("Circular dependency between packages 'a' and 'b'");
+  });
+
+  it("should throw when there is a circular dependency (a-> a, a -> a)", async () => {
+    const pkgA = { name: "a", version: "1.0.0", dependencies: { a: "1.0.0" } };
+    const monoRepoPkg = {
+      private: true,
+      workspaces: ["packages/**"],
+    };
+
+    mockFs({
+      "/package.json": JSON.stringify(monoRepoPkg),
+      "/packages/a/package.json": JSON.stringify(pkgA),
+    });
+
+    await expect(
+      async () =>
+        await findPackages(await findMonoRepo(), { order: "dependency" })
+    ).rejects.toThrowError("Circular dependency between packages 'a' and 'a'");
   });
 });
--- packages/utils/src/find-packages.ts
+++ packages/utils/src/find-packages.ts
@@ -3,8 +3,72 @@ import fs from "fs-extra";
 import glob from "glob-promise";
 import { MonoRepo, MonoRepoPackage, Package } from "./types";
 
+export interface FindPackagesOptions {
+  order?: "alphabetical" | "dependency";
+}
+
+const getOrderedDependencyPackages = (
+  pkg: MonoRepoPackage,
+  packages: MonoRepoPackage[],
+  resolvedPackages: MonoRepoPackage[] = [],
+  seenPackages: MonoRepoPackage[] = []
+): MonoRepoPackage[] => {
+  const localDependencyPackages = Object.keys(pkg.json?.dependencies)
+    .filter((dep) => packages.some((p) => p.json.name == dep))
+    .map((name) => packages.find((p) => p.json.name === name));
+
+  const seenPkgs = [...seenPackages];
+  let depPkgs = [...resolvedPackages];
+  for (let localDependencyPackage of localDependencyPackages) {
+    const seen = seenPkgs.find(
+      (d) => d.json.name === localDependencyPackage.json.name
+    );
+    if (seen) {
+      throw new Error(
+        `Circular dependency between packages '${seen.json.name}' and '${pkg.json.name}'`
+      );
+    }
+    seenPkgs.push(pkg);
+    const newDeps = getOrderedDependencyPackages(
+      localDependencyPackage,
+      packages,
+      depPkgs,
+      seenPkgs
+    );
+    for (let newDep of newDeps) {
+      const exists = depPkgs.find((d) => d.json.name === newDep.json.name);
+      if (exists) {
+        depPkgs = depPkgs.splice(depPkgs.indexOf(exists));
+      }
+      const seen = seenPkgs.find((d) => d.json.name === newDep.json.name);
+      if (seen) {
+        throw new Error(
+          `Ciruclar dependency between packages '${seen.json.name}' and '${newDep.json.name}'`
+        );
+      }
+
+      depPkgs.push(newDep);
+    }
+  }
+
+  const exists = depPkgs.find((d) => d.json.name === pkg.json.name);
+  if (exists) {
+    depPkgs = depPkgs.splice(depPkgs.indexOf(exists));
+  }
+  depPkgs.push(pkg);
+  const returnPackages = [];
+
+  for (let depPkg of depPkgs) {
+    if (!resolvedPackages.includes(depPkg)) {
+      returnPackages.push(depPkg);
+    }
+  }
+  return returnPackages;
+};
+
 export const findPackages = async (
-  monoRepo: MonoRepo
+  monoRepo: MonoRepo,
+  options: FindPackagesOptions = { order: "alphabetical" }
 ): Promise<MonoRepoPackage[]> => {
   // Build globs to packages package.json files
   const packageJsonGlobs = monoRepo.packageGlobs.map((x) =>
@@ -23,15 +87,44 @@ export const findPackages = async (
     .filter((x) => !x.includes("node_modules"));
 
   // Load all file contents into a JS object, it's JSON so serializes properly
-  const packages = await Promise.all(
+  const packageJsons = await Promise.all(
     packageJsonFilepaths.map((x: string): Promise<Package> => fs.readJson(x))
   );
 
   // Apply package meta data on the way out
-  return packages.map(
+  const packages = packageJsons.map(
     (x, i): MonoRepoPackage => ({
       dir: packageJsonFilepaths[i].replace("/package.json", ""),
       json: x,
     })
   );
+
+  switch (options.order) {
+    case "dependency": {
+      const rootDependencies = {};
+      for (let pkg of packages) {
+        rootDependencies[pkg.json.name] = pkg.json.version;
+      }
+
+      const rootPackage: MonoRepoPackage = {
+        dir: "",
+        json: {
+          name: "",
+          version: "1.0.0",
+          dependencies: rootDependencies,
+        },
+      };
+      const orderedPackages = getOrderedDependencyPackages(
+        rootPackage,
+        packages
+      );
+      return orderedPackages.filter((p) => p !== rootPackage);
+    }
+    case "alphabetical":
+    default: {
+      return packages.sort((a, b) =>
+        a.json.name.toLowerCase().localeCompare(b.json.name.toLowerCase())
+      );
+    }
+  }
 };
